0ece0706f6c98d676fd404c2ecfe95c896663c69
Renzo Stanley <renzo.stanley@sma.gob.cl>
2025-04-16 20:21:17 -0400

    cambio demostrativo de despliegue

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import AutorViewSet, LibroViewSet
 
 router = DefaultRouter()
-router.register(r"autores", AutorViewSet)
+router.register(r"autors", AutorViewSet)
 router.register(r"libros", LibroViewSet)
 
 urlpatterns = [
